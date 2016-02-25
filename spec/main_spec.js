e4aa94030be5523070d1aa0f8889ff903e97f901
邓楠 <dengnan@jiboyudeMacBook-Air.local>
2016-02-25 17:49:07 +0800

    Necessary files added to project

---
+++ spec/main_spec.js
@@ -0,0 +1,15 @@
+"use strict";
+var _ = require("lodash");
+var chai = require("chai");
+var sinon = require("sinon");
+var sinonChai = require("sinon-chai");
+var expect = chai.expect;
+chai.use(sinonChai);
+
+var main = require("../lib/main.js");
+
+
+describe("测试描述", function(){
+    sinon.spy(console, 'log');
+
+});
